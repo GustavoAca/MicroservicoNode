d385b9001d52295c812dee3950bfb71f22915af5
Gustavo <galasdalas50@gmail.com>
2022-02-09 14:35:47 -0300

    servidor completo

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("executei")
+
+import express,{ Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+
+app.get('/status', (req: Request, res: Response, next: NextFunction ) => {
+        res.status(200).send({ foo: 'Sucesso, bora pra cima'});
+} );
+
+
+app.listen(3000, () => {
+        console.log('Aplcicação executnato na porta 3000!');
+});
